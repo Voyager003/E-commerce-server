853f200c0cd37f8b65ba344cf805db94581fc9ba
voyager003 <miraclex000@gmail.com>
2026-01-20 20:34:47 +0900

    refactor: OAuth2UserInfoFactory에 GitHub 지원 추가 및 불필요한 주석 제거

--- src/main/java/ex/ecommerceserver/global/security/oauth2/OAuth2UserInfoFactory.java
+++ src/main/java/ex/ecommerceserver/global/security/oauth2/OAuth2UserInfoFactory.java
@@ -7,9 +7,7 @@ public class OAuth2UserInfoFactory {
     public static OAuth2UserInfo create(String registrationId, Map<String, Object> attributes) {
         return switch (registrationId.toLowerCase()) {
             case "google" -> GoogleOAuth2UserInfo.from(attributes);
-            // 추후 다른 OAuth2 제공자 추가
-            // case "kakao" -> KakaoOAuth2UserInfo.from(attributes);
-            // case "naver" -> NaverOAuth2UserInfo.from(attributes);
+            case "github" -> GitHubOAuth2UserInfo.from(attributes);
             default -> throw new IllegalArgumentException("지원하지 않는 OAuth2 제공자입니다: " + registrationId);
         };
     }
